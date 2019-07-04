4f5f8f269c537c72d5374cf95d8919c6e33ef222
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-03 21:05:02 -0700

    activitiy 26 node packages removed

--- week-10/day-03/class-activities/26-Inquirer_Users/Unsolved/userprompt.js
+++ week-10/day-03/class-activities/26-Inquirer_Users/Unsolved/userprompt.js
@@ -14,3 +14,46 @@
 // Remember to be creative!
 
 // ========================================================================
+// Load the NPM Package inquirer
+var inquirer = require("inquirer");
+
+// Create a "Prompt" with a series of questions.
+inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "input",
+      message: "What is your Stage name?",
+      name: "username"
+    },
+    // Here we create a basic password-protected text prompt.
+    {
+      type: "password",
+      message: "Set your passcode",
+      name: "password"
+    },
+    // Here we give the user a list to choose from.
+    {
+      type: "list",
+      message: "Which Stage lights do you prefer?",
+      choices: ["Spot-light", "Soft-Glow", "Rave-lights"],
+      name: "Lights"
+    },
+    // Here we ask the user to confirm.
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    }
+  ])
+  .then(function(inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and light setting from the answers.
+    if (inquirerResponse.confirm) {
+      console.log("\nWelcome " + inquirerResponse.username);
+      console.log("Your " + inquirerResponse.Lights + " Stage is ready for the show!\n");
+    }
+    else {
+      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you have faced your Stage Fright.\n");
+    }
+  });
