d968ef7210c6f9ed2bd5b03ffee05f85d03dd0e2
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-10 20:05:02 -0700

    created weird cli.js

--- week-11/day-03/class-activities/12-Stu_Cli/Unsolved/cli.js
+++ week-11/day-03/class-activities/12-Stu_Cli/Unsolved/cli.js
@@ -1,2 +1,29 @@
-// Write code here to parse command line arguments and store them into variables
-// Add code to print whether the user is searching for an actor or tv show, along with the name of the actor or tv show they are searching for
+var fs = require("fs");
+
+// Write code here to parse command line arguments 
+//and store them into variables
+var searchType = process.argv[2];
+var searchThis = process.argv[3];
+
+//call function to show results
+tvSearch(searchType, searchThis);
+
+function tvSearch(searchType, searchThis) {
+    switch (searchType) {
+        case "show":
+            showActorInfo(searchThis);
+            console.log(searchThis);
+            break;
+        case "actor":
+            showShowInfo(searchThis);
+            break;
+        default:
+            console.log("Invalid Search. \nPlease input first either 'show' or 'actor' \nand then the title of the show or the name of the actor \nyou wish to search.")
+    }
+}
+
+//fs how do i use fs in order to call the functions
+
+// Add code to print whether the user is searching 
+//for an actor or tv show, along with the name of 
+//the actor or tv show they are searching for
