ce2460fbed67bfdaf2956cca83104cabbfc92bc3
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-05 20:01:59 -0700

    secoond activity for the day

--- week-15/day-02/class-activities/08-Sequelize-Create-Read/Unsolved/routes/api-routes.js
+++ week-15/day-02/class-activities/08-Sequelize-Create-Read/Unsolved/routes/api-routes.js
@@ -14,15 +14,24 @@ module.exports = function(app) {
 
   // GET route for getting all of the todos
   app.get("/api/todos", function(req, res) {
-    // Write code here to retrieve all of the todos from the database and res.json them
-    // back to the user
+    // Write code here to retrieve all of the todos from the database 
+    // and res.json them back to the user
+    db.Todo.findAll({}).then(function(dbTodo) {
+      res.json(dbTodo);
+    });
   });
 
   // POST route for saving a new todo. We can create todo with the data in req.body
   app.post("/api/todos", function(req, res) {
-    // Write code here to create a new todo and save it to the database
-    // and then res.json back the new todo to the user
-
+    // Write code here to create a new todo and save
+    // it to the database and then res.json back the
+    // new todo to the user
+    db.Todo.create({
+      text: req.body.text,
+      complete: req.body.complete
+    }).then(function(dbTodo) {
+    res.json(dbTodo);
+    });
   });
 
   // DELETE route for deleting todos. We can get the id of the todo to be deleted from
