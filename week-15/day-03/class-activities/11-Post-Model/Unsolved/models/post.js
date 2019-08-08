88e5e60b922ce1997875273df4a9b5fc68ea830d
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-07 19:35:43 -0700

    create sequelize post js

--- week-15/day-03/class-activities/11-Post-Model/Unsolved/models/post.js
+++ week-15/day-03/class-activities/11-Post-Model/Unsolved/models/post.js
@@ -2,4 +2,41 @@ module.exports = function(sequelize, DataTypes) {
   // Add code here to create a Post model
   // This model needs a title, a body, and a category
   // Don't forget to 'return' the post after defining
+
+  var post = sequelize.define("post", {
+    title: {
+      type:DataTypes.STRING,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our todo is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our todo is between 1 and 140 characters
+      validate: {
+        len: [1, 800]
+      }
+    },
+    catagory: {
+      type: DataTypes.STRING,
+      // AllowNull is a flag that restricts a todo from being entered if it doesn't
+      // have a text value
+      allowNull: false,
+      // len is a validation that checks that our todo is between 1 and 140 characters
+      validate: {
+        len: [1, 140]
+      }
+    }
+  });
+  // DataTypes.STRING, a body property of 
+  // DataTypes.TEXT, and a category property of 
+  // DataTypes.STRING.
+  return post;
 };
