67da2d15386606194c810ad733bace402c36d5f4
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-09 19:16:08 -0700

    unfinished final activity

--- week-15/day-03/class-activities/13-Post-Author-Association/Unsolved/models/author.js
+++ week-15/day-03/class-activities/13-Post-Author-Association/Unsolved/models/author.js
@@ -3,5 +3,13 @@ module.exports = function(sequelize, DataTypes) {
     // Giving the Author model a name of type STRING
     name: DataTypes.STRING
   });
+
+  Author.associate = function(models) {\
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    Author.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
   return Author;
 };
