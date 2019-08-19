a01815520ccb51e50c17787681773d1cdbf0d152
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-08-18 21:06:49 -0700

    end of saturday class Mocha and Chai npm

--- week-17/day-01/class-activities/01-Stu_Warmup/Unsolved/tax.js
+++ week-17/day-01/class-activities/01-Stu_Warmup/Unsolved/tax.js
@@ -1,5 +1,9 @@
 var calculateTax = function(price) {
   // Fill in code here
+  if (typeof price !== "number") {
+    throw new Error ("Parameter `price` is required and must be a number.");
+  }
+  return Number((price * 1.08).toFixed(2));
 };
 
 module.exports = calculateTax;
