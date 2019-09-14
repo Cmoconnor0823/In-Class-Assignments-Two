b43e5a9b739401528bec7ce1326a0ea268538ced
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-14 11:06:56 -0700

    finished pupster app

--- week-20/day-01/06-Stu_PupsterApp/Solved/src/pages/Discover.js
+++ week-20/day-01/06-Stu_PupsterApp/Solved/src/pages/Discover.js
@@ -23,18 +23,22 @@ class Discover extends Component {
     const newState = { ...this.state };
 
     if (btnType === "pick") {
-      // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
+      // Set newState.match to either true or false depending on whether
+      // or not the dog likes us (1/5 chance)
       newState.match = 1 === Math.floor(Math.random() * 5) + 1;
 
-      // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
+      // Set newState.matchCount equal to its current
+      // value or its current value + 1 depending on whether the dog likes us
       newState.matchCount = newState.match
         ? newState.matchCount + 1
         : newState.matchCount;
     } else {
-      // If we thumbs down'ed the dog, we haven't matched with it
+      // If we thumbs down'ed the dog, we haven't
+      // matched with it
       newState.match = false;
     }
-    // Replace our component's state with newState, load the next dog image
+    // Replace our component's state with newState,
+    // load the next dog image
     this.setState(newState);
     this.loadNextDog();
   };
