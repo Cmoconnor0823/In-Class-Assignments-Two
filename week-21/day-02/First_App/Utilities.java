0e35bad8cd34cf6390b82ed1ee618c9dd581f461
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-21 20:22:51 -0700

    finished working examples

---
+++ week-21/day-02/First_App/Utilities.java
@@ -0,0 +1,12 @@
+package com.trilogy.app;
+
+import java.util.ArrayList;
+
+public class Utilities {
+	public static void getValues(ArrayList<String> arr) {
+		for (int i = 0; i < arr.size(); i++) {
+			System.out.println(arr.get(i));
+		}
+	}
+
+}
