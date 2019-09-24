34bbf29639b62bc4438c27ead4dc585c862054fc
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-23 19:14:53 -0700

    starting python

---
+++ week-21/day-03/test.py
@@ -0,0 +1,20 @@
+"""
+these are multi line comments
+"""
+# this is a single line comment
+
+name = "caitlin"
+print(name)
+age = 28.1
+print(age)
+print(type(age))
+#prints math out to the decimal 10 places
+print(1/3)
+#prints math divided rounded down
+print(1//3)
+
+print ("hello ",name,"!")
+
+#this will ask a question of the user and then print out the answer at the end of the second string
+mood = input("How are you today?")
+print("Great that you are feeling",mood)
